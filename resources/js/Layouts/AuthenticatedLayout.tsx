2909e794c5d3d1565eeba4f3ce9cc62314eaa95d
Irving Zamora <irvingzamora28@gmail.com>
2024-09-15 23:28:13 -0600

    feat: Add 'react-icons' dependency and utilized icons in AuthenticatedLayout for improved UI/UX and visual cues

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -4,6 +4,7 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
+import { HiMenu, HiX, HiChevronDown } from 'react-icons/hi';
 
 export default function Authenticated({ header, children }: PropsWithChildren<{ header?: ReactNode }>) {
     const user = usePage().props.auth.user;
@@ -45,18 +46,7 @@ export default function Authenticated({ header, children }: PropsWithChildren<{ 
                                         className="inline-flex w-full items-center px-4 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                     >
                                         {user.name}
-                                        <svg
-                                            className="ml-2 -mr-0.5 h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
+                                        <HiChevronDown className="ml-2 -mr-0.5 h-4 w-4" />
                                     </button>
                                 </span>
                             </Dropdown.Trigger>
@@ -78,29 +68,12 @@ export default function Authenticated({ header, children }: PropsWithChildren<{ 
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <div className="flex justify-between h-16">
                             <div className="flex items-center">
-                                {/* Toggle button for sidebar (left on large screens, far right on small screens) */}
+                                {/* Toggle button for sidebar (large screens) */}
                                 <button
-                                    className="hidden text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none lg:block lg:ml-8 lg:mr-2"
+                                    className="hidden text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none lg:block lg:ml-8 lg:mr-8"
                                     onClick={() => setIsSidebarOpen(!isSidebarOpen)}
                                 >
-                                    <svg
-                                        className="h-6 w-6"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        {isSidebarOpen ? (
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                                        ) : (
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M4 6h16M4 12h16M4 18h16"
-                                            />
-                                        )}
-                                    </svg>
+                                    {isSidebarOpen ? <HiX className="h-6 w-6" /> : <HiMenu className="h-6 w-6" />}
                                 </button>
 
                                 {/* Application logo in the navbar */}
@@ -116,31 +89,13 @@ export default function Authenticated({ header, children }: PropsWithChildren<{ 
                                 </div>
                             </div>
 
-                            {/* Move toggle button to far right for small screens */}
+                            {/* Toggle button for sidebar (small screens) */}
                             <div className="lg:hidden flex items-center">
-                                {/* Place the toggle button on the far right in small screens */}
                                 <button
                                     className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none"
                                     onClick={() => setIsSidebarOpen(!isSidebarOpen)}
                                 >
-                                    <svg
-                                        className="h-6 w-6"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        {isSidebarOpen ? (
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                                        ) : (
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M4 6h16M4 12h16M4 18h16"
-                                            />
-                                        )}
-                                    </svg>
+                                    {isSidebarOpen ? <HiX className="h-6 w-6" /> : <HiMenu className="h-6 w-6" />}
                                 </button>
                             </div>
 
@@ -155,18 +110,7 @@ export default function Authenticated({ header, children }: PropsWithChildren<{ 
                                                     className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                                 >
                                                     {user.name}
-                                                    <svg
-                                                        className="ml-2 -mr-0.5 h-4 w-4"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 20 20"
-                                                        fill="currentColor"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                            clipRule="evenodd"
-                                                        />
-                                                    </svg>
+                                                    <HiChevronDown className="ml-2 -mr-0.5 h-4 w-4" />
                                                 </button>
                                             </span>
                                         </Dropdown.Trigger>
